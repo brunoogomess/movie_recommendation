617ce48d14777566c6069649548d8f5b43a5fb0d
Bruno Gomes <brunomgprofissional@gmail.com>
2025-03-22 19:40:34 +0000

    added retrieval of movie title, id, popularity and rel date

--- api_movie_recommendation.py
+++ api_movie_recommendation.py
@@ -1,7 +1,16 @@
 import requests
 import credentials_file
 
+
+headers = {
+        "accept": "application/json",
+        "Authorization": credentials_file.bearer_token
+    }
+
 def get_genre_list():
+    '''
+    gets all the genres on the API
+    '''
     all_genres_url = 'https://api.themoviedb.org/3/genre/movie/list?language=en'
 
     headers = {
@@ -21,20 +30,44 @@ def get_genre_list():
 
 
 def ask_genre(genre_list, genre_dict):
+    '''
+    asks the user for the desired genre
+    providing the list of all  genres
+    '''
     chosen_genre = input(f"Please choose a genre from the following list:\n{genre_list}\n")
     chosen_genre = sorted(chosen_genre.title().split(" "))
     for genre in chosen_genre:
         if genre not in genre_list:
-            print(f"{genre} is not in the list. please choose a genre from the following list:\n{genre_list}\n")
+            print(f"{genre} is not in the list.") 
             return(ask_genre(genre_list, genre_dict))
-    print(chosen_genre)
-    get_movie_list(chosen_genre, genre_dict)
+        genre_id = genre_dict.get(genre)
+        
+        get_movie_list(genre_id)
 
-def get_movie_list(chosen_genre, genre_dict):
-    chosen_genre_id = genre_dict.keys()
-    print(chosen_genre_id)
-    #movies_filtered_by_genre_url = f'https://api.themoviedb.org/3/discover/movie?genre_id={genre_id}'
+def get_movie_list(genre_id):
+    '''
+    gets a list of movies (title:id)
+    that belong to the desired genre
+    '''
+    movie_data = []
+    #for page in range(1,1):
+        #movies_filtered_by_genre_url = f'https://api.themoviedb.org/3/discover/movie?page={page}&with_genres={genre_id}&language=en-US&sort_by=popularity.desc'
+    movies_filtered_by_genre_url = f'https://api.themoviedb.org/3/discover/movie?page=1&with_genres={genre_id}&language=en-US&sort_by=popularity.desc'
+    response = requests.get(movies_filtered_by_genre_url, headers=headers).json()
+    movie_data = [*movie_data, *response["results"]]
+    print(movie_data)
+    for movie in movie_data:
+        movie_dict = {"title": movie["title"], "id": movie["id"], "popularity": movie["popularity"], "release_date": movie["release_date"]}
+        print(movie_dict)
 
-
-
+        
 get_genre_list()
+
+
+
+
+'''
+TO DO:
+1. add release date
+3. should I convert this to a df and use the pandas code that i developed before?
+'''
